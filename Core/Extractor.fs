ad3e1e078de8081a5ea1d446f9063ee613cf8935
Alessandro <alessandro.piccione.75@gmail.com>
2018-11-13 23:21:25 +0000

    Implement parsing of date and time

--- Core/Extractor.fs
+++ Core/Extractor.fs
@@ -5,33 +5,44 @@ open System.Globalization
 open Entities
 
 
-// create an active pattern for the Date
-let (|Date|_|) (input:string) =
-    match DateTime.TryParseExact (input.Substring(0, 10), "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal) with
+// Active Patterns: https://docs.microsoft.com/en-us/dotnet/fsharp/language-reference/active-patterns 
+
+// create a partial active pattern with 2 arguments for the Date
+let (|Date|_|) (input:string, format:string) =
+    match DateTime.TryParseExact(input, format, CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal) with
     | true, date -> Some(date)
     | _ -> None
 
+//let (|Date|_|) (input:string) =
+//    match DateTime.TryParseExact(input, "", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal) with
+//    | true, date -> Some(date)
+//    | _ -> None
+
 let (|LogLevel|_|) (input:string) =
-    let pieces = input.Split(" ")
-    if pieces.Length > 1 then
-        match pieces.[1] with
+
+    match input with
         | "DEBUG" -> Some(LogLevel.Debug)
         | "INFO" -> Some(LogLevel.Info)
         | "WARN" -> Some(LogLevel.Warn)
         | "ERROR" -> Some(LogLevel.Error)
         | "FATAL" -> Some(LogLevel.Fatal)
         | _ -> None
-    else
-        None
 
-let extract line =
+
+// extract the Record from the single line
+let extract (line:string, dateFormat:string) =
+
+    let pieces = line.Split(' ', 5) // separators // date, time, thread, level, message
+
+
 
     let date = 
-        match line with
+        match (sprintf "%s %s" pieces.[0] pieces.[1]), dateFormat with
         | Date date -> date
         | _ -> DateTime.Now 
 
-    let logLevel = match line with 
+
+    let logLevel = match pieces.[2] with 
     | LogLevel level -> level
     | _ -> LogLevel.Info
 
--- Unit tests/Extractor test.fs
+++ Unit tests/Extractor test.fs
@@ -11,8 +11,10 @@ open LogsDataExtractor.Core.Entities
 let ``extract one line should return a Record`` () =
     
     let line = "this is a line"
+    let dateFormat = "yyyy-MM-dd HH:mm:ss"
 
-    let result = Extractor.extract line
+    let result = Extractor.extract(line, dateFormat)
+
     Assert.NotNull(result)
     Assert.True(typeof<Record> = result.GetType() )
     Assert.AreEqual(line, result.Message)
@@ -21,10 +23,12 @@ let ``extract one line should return a Record`` () =
 [<Test>]
 let ``extract the date from the line`` () =
     
-    let line = "9999-12-31 aaaaaaaaaaaaaaaaaa"
-    let expectedResult = new DateTime(9999, 12, 31)
+    let line = "9999-12-31 23:59:59 aaaaaaaaaaaaaaaaaa"
+    let dateFormat = "yyyy-MM-dd HH:mm:ss"
+    let expectedResult = new DateTime(9999, 12, 31, 23, 59, 59)
+    
+    let record = Extractor.extract( line, dateFormat)
 
-    let record = Extractor.extract line
     Assert.AreEqual(expectedResult, record.Date)
 
     
@@ -35,7 +39,9 @@ let ``extract the date from the line`` () =
 [<TestCase("FATAL", LogLevel.Fatal)>]
 let ``extract the log level from the line`` (level, logLevel:LogLevel) =
     
-    let line = sprintf "9999-12-31 %s aaaaaaaaaaaaaaaaaa" level
-        
-    let record = Extractor.extract line
+    let line = sprintf "9999-12-31 00:00:00 %s aaaaaaaaaaaaaaaaaa" level
+    let dateFormat = "yyyy-MM-dd HH:mm:ss"
+
+    let record = Extractor.extract(line, dateFormat)
+
     Assert.AreEqual( logLevel, record.Level)
