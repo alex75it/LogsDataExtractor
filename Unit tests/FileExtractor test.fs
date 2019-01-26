534b00af2229d97626b06527bb8c0864e6756028
Alessandro <alessandro.piccione.75@gmail.com>
2019-01-26 12:40:51 +0000

    Add unit test on FileExtractor

---
+++ Unit tests/FileExtractor test.fs
@@ -0,0 +1,18 @@
+﻿module tests.FileExtractor_test
+
+open NUnit.Framework
+open LogsDataExtractor.Core
+
+
+
+[<Test>][<Category("File extractor")>]
+let ``extract when file does not exists raise an error``() =
+
+    let file = "C://file that does not exists"
+    
+    let action = fun() -> FileExtractor.extract file |> ignore
+    //Assert.Throws<System.Exception>(action)  // does not work
+    Assert.Throws<System.Exception>(TestDelegate(action)) 
+    //Assert.Throws<System.Exception>( fun () -> FileExtractor.extract file |> ignore )
+    |> ignore
+
