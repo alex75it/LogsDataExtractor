429cf37ea8c8e7c4761bbfadd995cb49e59f56e9
Alessandro <alessandro.piccione.75@gmail.com>
2019-01-26 21:10:28 +0000

    File Extractor - Add test

--- Unit tests/FileExtractor test.fs
+++ Unit tests/FileExtractor test.fs
@@ -17,3 +17,14 @@ let ``extract when file does not exists raise an error``() =
     //Assert.Throws<System.Exception>( fun () -> FileExtractor.extract file |> ignore )
     |> ignore
 
+
+[<Test>][<Category("File extractor")>]
+let ``extract when there is a line return one record``() =
+
+    let file = "data/one line.log"
+
+    let records = FileExtractor.extract file
+
+    Assert.NotNull records
+    //Assert.That(records.Count = 1 , "message" + "aa")  // does not work
+    Assert.That(records.Count = 1 , sprintf "records count is %i" records.Count)
